c7fa5f0eebc3eec19a957221f5548fb5f6d0de41
Aman-Islam-2003 <amanislam019@gmail.com>
2023-11-23 23:54:26 +0500

    Update App.js

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,25 +1,14 @@
-import { useState } from "react";
+
+import Counter from "./Counter";
 
 function App(){
 
-  const [count, setCount] = useState(0);
-
-  let counter = 0
-  const addValue = ()=>{
-     setCount(counter+=1);
-     console.log(counter)
-  }
-  const decreaseValue = ()=>{
-    setCount(counter-=1);
-    console.log(counter)
- }
-  return(
-    <>
-      <h1>Counter Value: {count}</h1>
-      <button onClick={addValue}>Increment</button>
-      <button onClick={decreaseValue}>Decrement</button>
-    </>
-  )
+ return(
+  <>
+   {/* <Counter/>  */}
+   
+  </>
+ )
 }
 
 export default App;
