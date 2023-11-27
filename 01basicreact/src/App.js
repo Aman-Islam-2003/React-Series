ec85b298f2028502f799d2e9848eab07bc444bb1
Aman-Islam-2003 <amanislam019@gmail.com>
2023-11-27 23:47:57 +0500

    Update App.js

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,14 +1,21 @@
 
 import Card from "./components/Card";
 import Counter from "./components/Counter";
+import ColorChange from "./components/ColorChanger";
 
 function App(){
+  
+    const user = {
+       name:"Aman",
+       age:20,
+    }
 
  return(
   <>
+  <ColorChange/>
    {/* <Counter/>  */}
-   < Card/>
-   <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+   {/* < Card  name={user.name} age={user.age} />
+   <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1> */}
   </>
  )
 }
