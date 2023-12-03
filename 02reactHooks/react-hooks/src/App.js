a2f6977173052474b6b8a91beb3546c6319a4c8b
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-03 21:13:54 +0500

    Update App.js

--- 02reactHooks/react-hooks/src/App.js
+++ 02reactHooks/react-hooks/src/App.js
@@ -1,43 +1,83 @@
-
-import { useCallback, useState } from 'react';
-import './App.css';
-import './index.css';
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState(" ")
+  const [password, setPassword] = useState(" ");
 
-  const passwordGenerator = useCallback((array)=>{
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(numberAllowed){
-      str += "0123456789"
-    }
+  //userRef
+  const passwordRef = useRef(null);
 
-    if(charAllowed){
-      str += "@$%`{}[]+-_&*^%"
-    }
+  const passwordGenerator = useCallback(
+    () => {
+      let pass = "";
+      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      if (numberAllowed) {
+        str += "0123456789";
+      }
 
-    for (let i = 0; i < array.length; i++) {
-      let randomChar = Math.floor(Math.random()*str.length+1);
-      pass = str.charAt(randomChar);
-    }
+      if (charAllowed) {
+        str += "@$%`{}[]+-_&*^%";
+      }
 
-    setPassword(pass )
+      for (let i = 0; i < length; i++) {
+        let randomChar = Math.floor(Math.random() * str.length + 1);
+        pass += str.charAt(randomChar);
+      }
+
+      setPassword(pass);
+    },
+    [length, charAllowed, numberAllowed, setPassword]
+  );
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+   window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(()=>{
+     passwordGenerator();
   },[length, charAllowed, numberAllowed, setPassword])
 
-  
   return (
     <>
-     <div>
-      <h3 className='heading'>Password Generator</h3>
+      <div>
+        <h3 className="heading">Password Generator</h3>
       </div>
 
       <div>
-        <input type='text' value={password} className='' readOnly placeholder='password'/>
-        <button>Copy</button>
+        <input
+          type="text"
+          value={password}
+          className=""
+          readOnly
+          placeholder="password"
+          ref={passwordRef}
+        />
+        <button onClick={copyPasswordToClipboard}>Copy</button>
+      </div>
+
+      <div>
+        <input type="range" min={6} max={50} value={length} className="cursor-pointer" onChange={(e)=>{
+          setLength(e.target.value)
+        }}/>
+        <label>Length: {length}</label>
+      </div>
+      <div>
+      <input type="checkbox" defaultChecked={numberAllowed} id="numberInput" onChange={(e)=>{
+          setNumberAllowed((prev)=>!prev)
+        }}/>
+        <label>Numbers</label>
+      </div>
+
+      <div>
+      <input type="checkbox" defaultChecked={charAllowed} id="charInput" onChange={(e)=>{
+          setCharAllowed((prev)=>!prev)
+        }}/>
+        <label>Characters</label>
       </div>
     </>
   );
