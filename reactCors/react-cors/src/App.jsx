3e2921ecbf6cba318b6684eee9bb4f5716693aa8
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-10 23:36:38 +0500

    Update App.jsx

--- reactCors/react-cors/src/App.jsx
+++ reactCors/react-cors/src/App.jsx
@@ -2,35 +2,43 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [form , setForm] = useState({});
+  const [form, setForm] = useState({});
 
-   const formHandler = (event)=>{
+  const formHandler = (event) => {
     //console.log(event.target.value, event.target.name);
     setForm({
       ...form,
       [event.target.name]: event.target.value
     })
-   }
+  }
 
-   const handleSubmit = (event)=>{
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    //use fetch for api call
-    
+    const response = await fetch('http://localhost:8080/demo', {
+      method: 'POST',
+      body: JSON.stringify(form),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    });
+    console.log(response);
+   const data = await response.text();
 
+   console.log(data);
 
-   }
+  }
 
   return (
     <div className='container'>
       <div className='wrapper'>
         <form onSubmit={handleSubmit}>
           <label>UserName</label>
-          <input type='text' placeholder='Username' name='username' onChange={formHandler}/>
+          <input type='text' placeholder='Username' name='username' onChange={formHandler} />
           <label>Password</label>
-          <input type='password' placeholder='Password' name='password' onChange={formHandler}/>
+          <input type='password' placeholder='Password' name='password' onChange={formHandler} />
 
-          <input type='submit'/>
+          <input type='submit' />
 
         </form>
       </div>
