9d9313d80c39acf47aaf149c6da08f26731f2ddf
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-10 23:36:27 +0500

    Update index.js

--- reactCors/nodeServer/index.js
+++ reactCors/nodeServer/index.js
@@ -1,17 +1,45 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const mongoose = require("mongoose");
 const PORT = 8080;
 
-app.get('/demo', (req,res)=>{
-    try{
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
+main().catch((err) => console.log(err));
 
-    }catch(err){
-        console.log(err);
-    }
-})
+async function main() {
+  mongoose.connect(
+    "mongodb+srv://farrukhadeel:pps993icp956@cluster0.ysuiv.mongodb.net/demo"
+  );
+  console.log("db connected");
+}
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`)
-})
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("users", userSchema);
+
+app.post("/demo", async (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(500).send("Required fields can't be empty");
+  }
+
+  let user = new User({
+    username,
+    password,
+  });
+
+  const doc = await user.save();
+  console.log(doc);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
