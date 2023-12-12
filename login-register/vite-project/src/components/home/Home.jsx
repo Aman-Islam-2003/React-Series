0b13edab948ee3307c78da0339495e721f73917e
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-12 20:52:16 +0500

    Create Home.jsx

---
+++ login-register/vite-project/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='wrapper'>
+            <div className='homepage'>
+                <h2>Hello HomePage</h2>
+                <button>Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
