2a94ba63a180755f7a7ad8ad4999421460fd457a
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-12 20:51:41 +0500

    Update App.jsx

--- login-register/vite-project/src/App.jsx
+++ login-register/vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import './App.css'
+import Login from './components/login/Login'
+import Home from './components/home/Home'
+import Register from './components/register/Register'
 
 function App() {
 
   return (
     <>
-      
+      <Register />
+      {/* <Login/> */}
     </>
   )
 }
