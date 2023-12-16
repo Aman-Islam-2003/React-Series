dee914604bf7d2b28d0a65b5668a70f76420726a
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-16 15:42:17 +0500

    Update Register.jsx

--- login-register/vite-project/src/components/register/Register.jsx
+++ login-register/vite-project/src/components/register/Register.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import './Register.css'
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     name: '',
     email:'',
@@ -21,8 +23,19 @@ const Register = () => {
   const register = (event)=>{
     const { name, email, password, reEnterPassword } = user;
     if (name && email && password && (password === reEnterPassword)) {
-      axios.post("http://localhost:4000/register", user).then((res)=> console.log(res))
-    }else{
+      // axios.post("http://localhost:4000/register", user).then((res)=> console.log(res))
+
+      fetch("http://localhost:4000/register", {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json' 
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  }else{
       alert("Invalid input")
     }
 
@@ -32,11 +45,15 @@ const Register = () => {
     <div className='wrapper'>
     <div className='register'>
         <h1>Register</h1>
-        <input type='text' name="name" placeholder='Enter your name' onChange={handleInput}/>
-            <input type='email' name="email" placeholder='Enter your email' onChange={handleInput}/>
-            <input type='password'  name="password" placeholder='Enter your password' onChange={handleInput}/>
-            <input type='password'  name="reEnterPassword" placeholder='ReEnter your password' onChange={handleInput}/>
+        <input type='text' name="name" value={user.name} placeholder='Enter your name' onChange={handleInput}/>
+            <input type='email' name="email"value={user.email}  placeholder='Enter your email' onChange={handleInput}/>
+            <input type='password'  name="password" value={user.password} placeholder='Enter your password' onChange={handleInput}/>
+            <input type='password'  name="reEnterPassword" value={user.reEnterPassword} placeholder='ReEnter your password' onChange={handleInput}/>
             <button onClick={register}>Register</button>
+            <div>or</div>
+            <button onClick={()=>{
+                navigate('/login')
+            }}>Login</button>
     </div>
     </div>
   )
