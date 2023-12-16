da633ab9274f2302fe9daaf63935f98ad627d2fa
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-16 15:42:06 +0500

    Update App.jsx

--- login-register/vite-project/src/App.jsx
+++ login-register/vite-project/src/App.jsx
@@ -3,13 +3,21 @@ import './App.css'
 import Login from './components/login/Login'
 import Home from './components/home/Home'
 import Register from './components/register/Register'
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [user, setLoginUser] = useState({})
 
   return (
     <>
-      <Register />
-      {/* <Login/> */}
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
