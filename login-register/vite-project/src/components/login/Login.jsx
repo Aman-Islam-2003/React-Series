caae2ba0f053fa287f7d0d930fad8904ca0e458a
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-16 15:42:12 +0500

    Update Login.jsx

--- login-register/vite-project/src/components/login/Login.jsx
+++ login-register/vite-project/src/components/login/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import './Login.css'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState({
     email:'',
     password:'',
@@ -12,18 +14,37 @@ const Login = () => {
       ...user,
       [event.target.name]: event.target.value,
     })
-    console.log(event.target.name, event.target.value)
+    //console.log(event.target.name, event.target.value)
+  }
+
+  const login = (event)=>{
+  
+      // axios.post("http://localhost:4000/login", user).then((res)=> console.log(res))
+
+      fetch("http://localhost:4000/login", {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json' 
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
   }
 
   return (
     <div className='wrapper'>
     <div className='login'>
         <h1>Login</h1>
-            <input type='email'  name="email" placeholder='Enter your email' onChange={handleInput}/>
-            <input type='password' name="password"  placeholder='Enter your password' onChange={handleInput}/>
-            <button>Login</button>
+            <input type='email'  name="email" value={user.email} placeholder='Enter your email' onChange={handleInput}/>
+            <input type='password' name="password" value={user.password} placeholder='Enter your password' onChange={handleInput}/>
+            <button onClick={login}>Login</button>
             <div>or</div>
-            <button>Register</button>
+            <button onClick={()=>{
+                navigate('/register')
+            }}>Register</button>
     </div>
     </div>
   )
