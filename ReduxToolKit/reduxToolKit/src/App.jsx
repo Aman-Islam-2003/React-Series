f6f21cd540b0d3070196c9db684b30a06ad5f735
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-20 00:01:02 +0500

    Update App.jsx

--- ReduxToolKit/reduxToolKit/src/App.jsx
+++ ReduxToolKit/reduxToolKit/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Todos from './components/Todos'
+import AddToDo from './components/addToDo'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <AddToDo/>
+    <Todos/>
     </>
   )
 }
