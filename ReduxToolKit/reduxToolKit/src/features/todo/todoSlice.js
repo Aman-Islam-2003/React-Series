b0a9ce6f920881351cf62196342c84278d602ec4
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-20 00:01:13 +0500

    Update todoSlice.js

--- ReduxToolKit/reduxToolKit/src/features/todo/todoSlice.js
+++ ReduxToolKit/reduxToolKit/src/features/todo/todoSlice.js
@@ -1,30 +1,30 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{
-        id:1,
-        text:"Hello World"
-    }]
-}
+  todos: [
+    {
+      id: 1,
+      text: "Hello World",
+    },
+  ],
+};
 
 export const todoSlice = createSlice({
-    name:'todo',
-    initialState,
-    reducers: {
-        addToDo: (state, action)=>{
-           const todo= {
-            id: nanoid(),
-            text: action.payload.text
-           }
-           state.todos.push(todo)
-        },
-        removeToDo: (state, action)=>{
-           state.todos = state.todos.filter((todo)=>{
-            todo.id !== action.payload.id
-           })
-        }
-    }
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addToDo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeToDo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
 
-export const {addToDo, removeToDo} = todoSlice.actions;
-export default todoSlice.reducer
+export const { addToDo, removeToDo } = todoSlice.actions; //used in components
+export default todoSlice.reducer;
