18835c3f17cc0d00601324d7d1d6e0f7c9b5487a
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-23 23:38:47 +0500

    Update App.jsx

--- MultiFormTask/multiforms/src/App.jsx
+++ MultiFormTask/multiforms/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SignUp from './components/SignUp'
+import Topnav from './components/Topnav'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SignUp />
+      {/* <SignUp /> */}
+      {/* <Login/> */}
+      <Topnav/>
     </>
   )
 }
