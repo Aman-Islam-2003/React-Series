cce85a94e72a00b2f9af44e7caa5eaa5f95ed0f6
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-23 23:39:07 +0500

    Update formSlice.js

--- MultiFormTask/multiforms/src/features/formSlice.js
+++ MultiFormTask/multiforms/src/features/formSlice.js
@@ -1,21 +1,21 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   trace: 0,
-// };
+const initialState = {
+  trace: 0,
+};
 
-// export const formSlice = createSlice({
-//   name: "form",
-//   initialState,
-//   reducers: {
-//     moveNextAction: (state, action)=>{
-//       state.trace = state.trace + 1;
-//     },
-//     movePrevAction: (state, action)=>{
-//         state.trace = state.trace - 1;
-//       }
-//   },
-// });
+export const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    moveNextAction: (state)=>{
+      state.trace = state.trace + 1;
+    },
+    movePrevAction: (state)=>{
+        state.trace = state.trace - 1;
+      }
+  },
+});
 
-// export const {moveNextAction, movePrevAction} = formSlice.actions;
-// export default formSlice.reducer;
+export const {moveNextAction, movePrevAction} = formSlice.actions;
+export default formSlice.reducer;
