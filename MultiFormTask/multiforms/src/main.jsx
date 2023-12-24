0411f970b185f21c4693c5e0a16107da55df6d9b
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-24 22:40:48 +0500

    Update main.jsx

--- MultiFormTask/multiforms/src/main.jsx
+++ MultiFormTask/multiforms/src/main.jsx
@@ -4,9 +4,39 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import {store} from './app/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import SignUp from './components/SignUp.jsx'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
+import Layout from './components/Layout.jsx'
+import Track from './components/Track.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    element: <Track />,
+    children: [
+      {
+        path: '',
+        element: <SignUp />,
+      }, {
+        path: 'login',
+        element: <Login/>,
+      },
+      {
+        path: 'profile',
+        element: <Profile/>,
+      }
+    ]
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <App />
+  <RouterProvider router={router} />
 </Provider>,
 )
