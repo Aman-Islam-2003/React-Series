e748edbc9f4142cc4801086fd1ca6c08c545a814
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-24 22:41:00 +0500

    Update Topnav.jsx

--- MultiFormTask/multiforms/src/components/Topnav.jsx
+++ MultiFormTask/multiforms/src/components/Topnav.jsx
@@ -5,27 +5,34 @@ const Topnav = () => {
   const state = useSelector((state) => state);
 
   return (
-    <div className='bg-white w-100 rounded-md h-5 flex justify-around mb-10'>
-      {state.trace === 0 && (
+    <div className='bg-black w-100 rounded-md h-5 flex justify-around mb-10'>
+      {(state.trace > 2) && (
          <>
-         <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
-         <div className='bg-white m-auto w-20 inline-block rounded-md h-2'></div>
-         <div className='bg-white m-auto w-20 inline-block rounded-md h-2'></div>
+         <div className='bg-black m-auto w-20 inline-block rounded-md h-2'></div>
+         <div className='bg-black m-auto w-20 inline-block rounded-md h-2'></div>
+         <div className='bg-black m-auto w-20 inline-block rounded-md h-2'></div>
        </>
         
       )}
-      {state.trace === 1 && (
+      {state.trace === 0 && (
         <>
-          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
-          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
-          <div className='bg-white m-auto w-20 inline-block rounded-md h-2'></div>
+          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-5'></div>
+          <div className='bg-black  m-auto w-20 inline-block rounded-md h-2'></div>
+          <div className='bg-black m-auto w-20 inline-block rounded-md h-2'></div>
         </>
       )}
-      {state.trace === 2 && (
+      {state.trace === 1 && (
         <>
-          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
-          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
-          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-2'></div>
+          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-5'></div>
+          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-5'></div>
+          <div className='bg-black m-auto w-20 inline-block rounded-md h-5'></div>
+        </>
+      )}
+         {state.trace === 2 && (
+        <>
+          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-5'></div>
+          <div className='bg-purple-500 m-auto w-20 inline-block rounded-md h-5'></div>
+          <div className='bg-purple-500  m-auto w-20 inline-block rounded-md h-5'></div>
         </>
       )}
     </div>
